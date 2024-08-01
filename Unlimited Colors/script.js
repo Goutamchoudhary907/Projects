73b8eae4cbe18e30ccafd87053ce91ad5ecd1b41
Goutam Choudhary <goutamchoudhary907@gmail.com>
2024-08-01 23:15:19 +0530

    commit

---
+++ Unlimited Colors/script.js
@@ -0,0 +1,28 @@
+// generate a random color 
+
+const randomColor=function(){
+    const hex='0123456789ABCDEF';
+    let color='#';
+
+    for (let i = 0; i <6; i++) {
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+ let intervalId;
+const startChangingColor=function(){
+    if(!intervalId){                                                                      // check if intervalId =null then change bgcolor 
+        intervalId= setInterval(changebgColor,1000);
+    }
+    function changebgColor(){
+        document.body.style.backgroundColor=randomColor();
+    }
+};
+const stopChangingColor=function(){
+  clearInterval(intervalId);
+  intervalId= null;                                                                       // after stop interval ,set intervalId=null 
+};
+
+
+document.querySelector('#start').addEventListener('click',startChangingColor)
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
